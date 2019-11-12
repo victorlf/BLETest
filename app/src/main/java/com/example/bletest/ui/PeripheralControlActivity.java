ff9ffe87e198279c165792705089e1e5e77f3409
victorlf <victorlfonseca@gmail.com>
2019-11-11 21:17:47 -0300

    Classe BleAdapterService
    Peripheral Control Screen
    
    Connecting to the Peripheral Device

--- app/src/main/java/com/example/bletest/ui/PeripheralControlActivity.java
+++ app/src/main/java/com/example/bletest/ui/PeripheralControlActivity.java
@@ -2,9 +2,11 @@ package com.example.bletest.ui;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.bluetooth.BluetoothGattService;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
@@ -13,6 +15,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.LinearLayout;
 import android.widget.Switch;
 import android.widget.TextView;
 
@@ -20,7 +23,9 @@ import com.example.bletest.Constants;
 import com.example.bletest.R;
 import com.example.bletest.bluetooth.BleAdapterService;
 
+import java.util.List;
 import java.util.Timer;
+import java.util.TimerTask;
 
 public class PeripheralControlActivity extends Activity {
     public static final String EXTRA_NAME = "name";
@@ -71,14 +76,137 @@ public class PeripheralControlActivity extends Activity {
                             .findViewById(R.id.connectButton)).setEnabled(false);
                     // we're connected
                     showMsg("CONNECTED");
+                    // enable the LOW/MID/HIGH alert level selection buttons
+                    ((Button)
+                            PeripheralControlActivity.this.findViewById(R.id.lowButton)).setEnabled(true);
+                    ((Button)
+                            PeripheralControlActivity.this.findViewById(R.id.midButton)).setEnabled(true);
+                    ((Button)
+                            PeripheralControlActivity.this.findViewById(R.id.highButton)).setEnabled(true);
+                    // Added for serivce descovering part
+                    bluetooth_le_adapter.discoverServices();
                     break;
                 case BleAdapterService.GATT_DISCONNECT:
                     ((Button) PeripheralControlActivity.this
                             .findViewById(R.id.connectButton)).setEnabled(true);
                     // we're disconnected
                     showMsg("DISCONNECTED");
+                    // hide the rssi distance colored rectangle
+                    ((LinearLayout) PeripheralControlActivity.this
+                            .findViewById(R.id.rectangle))
+                            .setVisibility(View.INVISIBLE);
+                    // disable the LOW/MID/HIGH alert level selection buttons
+                    ((Button)
+                            PeripheralControlActivity.this.findViewById(R.id.lowButton)).setEnabled(false);
+                    ((Button)
+                            PeripheralControlActivity.this.findViewById(R.id.midButton)).setEnabled(false);
+                    ((Button)
+                            PeripheralControlActivity.this.findViewById(R.id.highButton)).setEnabled(false);
+                    // stop the rssi reading timer
+                    stopTimer();
                     if (back_requested) {
                         PeripheralControlActivity.this.finish();
+                    }
+                    break;
+                case BleAdapterService.GATT_SERVICES_DISCOVERED:
+                    // validate services and if ok....
+                    List<BluetoothGattService> slist =
+                            bluetooth_le_adapter.getSupportedGattServices();
+                    boolean link_loss_present=false;
+                    boolean immediate_alert_present=false;
+                    boolean tx_power_present=false;
+                    boolean proximity_monitoring_present=false;
+                    boolean health_thermometer_present = false;
+
+                    for (BluetoothGattService svc : slist) {
+                        Log.d(Constants.TAG, "UUID=" + svc.getUuid().toString().toUpperCase() + " " +
+                                "INSTANCE=" + svc.getInstanceId());
+                        if
+                        (svc.getUuid().toString().equalsIgnoreCase(BleAdapterService.LINK_LOSS_SERVICE_UUID)) {
+                            link_loss_present = true;
+                            continue;
+                        }
+                        if
+                        (svc.getUuid().toString().equalsIgnoreCase(BleAdapterService.IMMEDIATE_ALERT_SERVICE_UUID)) {
+                            immediate_alert_present = true;
+                            continue;
+                        }
+                        if
+                        (svc.getUuid().toString().equalsIgnoreCase(BleAdapterService.TX_POWER_SERVICE_UUID)) {
+                            tx_power_present = true;
+                            continue;
+                        }
+                        if
+                        (svc.getUuid().toString().equalsIgnoreCase(BleAdapterService.PROXIMITY_MONITORING_SERVICE_UUID))
+                        {
+                            proximity_monitoring_present = true;
+                            continue;
+                        }
+                        if
+                        (svc.getUuid().toString().equalsIgnoreCase(BleAdapterService.HEALTH_THERMOMETER_SERVICE_UUID)) {
+                            health_thermometer_present = true;
+                            continue;
+                        }
+                    }
+                    if (link_loss_present && immediate_alert_present && tx_power_present &&
+                            proximity_monitoring_present && health_thermometer_present) {
+                        showMsg("Device has expected services");
+                        // show the rssi distance colored rectangle
+                        ((LinearLayout) PeripheralControlActivity.this
+                                .findViewById(R.id.rectangle))
+                                .setVisibility(View.VISIBLE);
+                        // enable the LOW/MID/HIGH alert level selection buttons
+                        ((Button)
+                                PeripheralControlActivity.this.findViewById(R.id.lowButton)).setEnabled(true);
+                        ((Button)
+                                PeripheralControlActivity.this.findViewById(R.id.midButton)).setEnabled(true);
+                        ((Button)
+                                PeripheralControlActivity.this.findViewById(R.id.highButton)).setEnabled(true);
+
+                        bluetooth_le_adapter.readCharacteristic(
+                                BleAdapterService.LINK_LOSS_SERVICE_UUID,
+                                BleAdapterService.ALERT_LEVEL_CHARACTERISTIC);
+
+                    } else {
+                        showMsg("Device does not have expected GATT services");
+                    }
+                    break;
+                case BleAdapterService.GATT_CHARACTERISTIC_READ:
+                    bundle = msg.getData();
+                    Log.d(Constants.TAG, "Service=" +
+                            bundle.get(BleAdapterService.PARCEL_SERVICE_UUID).toString().toUpperCase() + " Characteristic=" +
+                            bundle.get(BleAdapterService.PARCEL_CHARACTERISTIC_UUID).toString().toUpperCase());
+                    if (bundle.get(BleAdapterService.PARCEL_CHARACTERISTIC_UUID).toString()
+                            .toUpperCase().equals(BleAdapterService.ALERT_LEVEL_CHARACTERISTIC)
+                            && bundle.get(BleAdapterService.PARCEL_SERVICE_UUID).toString()
+                            .toUpperCase().equals(BleAdapterService.LINK_LOSS_SERVICE_UUID)) {
+                        b = bundle.getByteArray(BleAdapterService.PARCEL_VALUE);
+                        if (b.length > 0) {
+                            PeripheralControlActivity.this.setAlertLevel((int) b[0]);
+                            // show the rssi distance colored rectangle
+                            ((LinearLayout) PeripheralControlActivity.this
+                                    .findViewById(R.id.rectangle))
+                                    .setVisibility(View.VISIBLE);
+                            // start off the rssi reading timer
+                            startReadRssiTimer();
+                        }
+                    }
+                    break;
+                case BleAdapterService.GATT_REMOTE_RSSI:
+                    bundle = msg.getData();
+                    int rssi = bundle.getInt(BleAdapterService.PARCEL_RSSI);
+                    PeripheralControlActivity.this.updateRssi(rssi);
+                    break;
+                case BleAdapterService.GATT_CHARACTERISTIC_WRITTEN:
+                    bundle = msg.getData();
+                    if (bundle.get(BleAdapterService.PARCEL_CHARACTERISTIC_UUID).toString()
+                            .toUpperCase().equals(BleAdapterService.ALERT_LEVEL_CHARACTERISTIC)
+                            && bundle.get(BleAdapterService.PARCEL_SERVICE_UUID).toString()
+                            .toUpperCase().equals(BleAdapterService.LINK_LOSS_SERVICE_UUID)) {
+                        b = bundle.getByteArray(BleAdapterService.PARCEL_VALUE);
+                        if (b.length > 0) {
+                            PeripheralControlActivity.this.setAlertLevel((int) b[0]);
+                        }
                     }
                     break;
             }
@@ -97,6 +225,11 @@ public class PeripheralControlActivity extends Activity {
 
         // show the device name
         ((TextView) this.findViewById(R.id.nameTextView)).setText("Device : "+device_name+" " + "["+device_address+"]");
+
+        // hide the coloured rectangle used to show green/amber/red rssi
+        // distance
+        ((LinearLayout) this.findViewById(R.id.rectangle))
+                .setVisibility(View.INVISIBLE);
 
         // disable the noise button
         ((Button) PeripheralControlActivity.this.findViewById(R.id.noiseButton))
@@ -121,6 +254,7 @@ public class PeripheralControlActivity extends Activity {
         showMsg("READY");
     }
 
+    // TextView above the button MAKE A NOISE
     private void showMsg(final String msg) {
         Log.d(Constants.TAG, msg);
         runOnUiThread(new Runnable() {
@@ -136,17 +270,31 @@ public class PeripheralControlActivity extends Activity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        // Stop the rssi reading timer when activity is destroied
+        stopTimer();
         unbindService(service_connection);
         bluetooth_le_adapter = null;
     }
 
     public void onLow(View view) {
+        bluetooth_le_adapter.writeCharacteristic(
+                BleAdapterService.LINK_LOSS_SERVICE_UUID,
+                BleAdapterService.ALERT_LEVEL_CHARACTERISTIC, Constants.ALERT_LEVEL_LOW
+        );
     }
 
     public void onMid(View view) {
+        bluetooth_le_adapter.writeCharacteristic(
+                BleAdapterService.LINK_LOSS_SERVICE_UUID,
+                BleAdapterService.ALERT_LEVEL_CHARACTERISTIC, Constants.ALERT_LEVEL_MID
+        );
     }
 
     public void onHigh(View view) {
+        bluetooth_le_adapter.writeCharacteristic(
+                BleAdapterService.LINK_LOSS_SERVICE_UUID,
+                BleAdapterService.ALERT_LEVEL_CHARACTERISTIC, Constants.ALERT_LEVEL_HIGH
+        );
     }
 
     public void onNoise(View view) {
@@ -178,4 +326,64 @@ public class PeripheralControlActivity extends Activity {
             finish();
         }
     }
+
+    private void setAlertLevel(int alert_level) {
+        this.alert_level = alert_level;
+        ((Button) this.findViewById(R.id.lowButton)).setTextColor(Color.parseColor("#000000")); ;
+        ((Button) this.findViewById(R.id.midButton)).setTextColor(Color.parseColor("#000000")); ;
+        ((Button) this.findViewById(R.id.highButton)).setTextColor(Color.parseColor("#000000"));
+        ;
+        switch (alert_level) {
+            case 0:
+                ((Button)
+                        this.findViewById(R.id.lowButton)).setTextColor(Color.parseColor("#FF0000")); ;
+                break;
+            case 1:
+                ((Button)
+                        this.findViewById(R.id.midButton)).setTextColor(Color.parseColor("#FF0000")); ;
+                break;
+            case 2:
+                ((Button)
+                        this.findViewById(R.id.highButton)).setTextColor(Color.parseColor("#FF0000")); ;
+                break;
+        }
+    }
+
+    // Read RSSI
+    // Initiates regular sampling of the RSSI
+    private void startReadRssiTimer() {
+        mTimer = new Timer();
+        mTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                bluetooth_le_adapter.readRemoteRssi();
+            }
+        }, 0, 2000);
+    }
+    // Stops the sampling process
+    private void stopTimer() {
+        if (mTimer != null) {
+            mTimer.cancel();
+            mTimer = null;
+        }
+    }
+    // Updates the UI with RSSI values
+    private void updateRssi(int rssi) {
+        ((TextView) findViewById(R.id.rssiTextView)).setText("RSSI = "
+                + Integer.toString(rssi));
+        LinearLayout layout = ((LinearLayout) PeripheralControlActivity.this
+                .findViewById(R.id.rectangle));
+        byte proximity_band = 3;
+        if (rssi < -80) {
+            layout.setBackgroundColor(0xFFFF0000);
+        } else if (rssi < -50) {
+            layout.setBackgroundColor(0xFFFF8A01);
+            proximity_band = 2;
+        } else {
+            layout.setBackgroundColor(0xFF00FF00);
+            proximity_band = 1;
+        }
+        layout.invalidate();
+    }
+
 }
