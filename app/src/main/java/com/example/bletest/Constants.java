41a7706d539959599fe27199b1c120c39dffd60a
victorlf <victorlfonseca@gmail.com>
2019-11-11 21:18:09 -0300

    Classe BleAdapterService
    Peripheral Control Screen
    
    Connecting to the Peripheral Device

--- app/src/main/java/com/example/bletest/Constants.java
+++ app/src/main/java/com/example/bletest/Constants.java
@@ -5,4 +5,8 @@ public class Constants {
     public static final String FIND = "Find BDSK Devices";
     public static final String STOP_SCANNING = "Stop Scanning";
     public static final String SCANNING = "Scanning";
+
+    public static final byte [] ALERT_LEVEL_LOW = { (byte) 0x00};
+    public static final byte [] ALERT_LEVEL_MID = { (byte) 0x01};
+    public static final byte [] ALERT_LEVEL_HIGH = { (byte) 0x02};
 }
